edcf42ae3b0457462c4db0bdb51ac7c97aadc024
Vitor Silva <sirmochi45@gmail.com>
2020-03-19 16:02:29 -0300

    Initial Commit

---
+++ Commet Assault/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Commet Assault
+//
+//  Created by Vitor Silva on 19/03/20.
+//  Copyright © 2020 Vitor Silva. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
